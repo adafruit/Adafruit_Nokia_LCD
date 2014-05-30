bf848c8716074314b3b96731228ea28541c54a9f
Tony DiCola <tony@tonydicola.com>
2014-05-30 13:39:49 +0000

    Add dependency on Adafruit-GPIO library.

--- setup.py
+++ setup.py
@@ -9,4 +9,6 @@ setup(name 				= 'Adafruit_Nokia_LCD',
 	  description		= 'Library to display images on the Nokia 5110/3110 LCD.',
 	  license			= 'MIT',
 	  url				= 'https://github.com/adafruit/Adafruit_Nokia_LCD/',
+	  dependency_links	= ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.1.0'],
+	  install_requires	= ['Adafruit-GPIO>=0.1.0'],
 	  packages 			= find_packages())
